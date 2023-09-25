56478746e0ff0d30a039fc08190d345a7964c5c0
milovanpinxteren <milo.van.pinxteren@gmail.com>
2023-09-25 18:48:40 +0200

    feat: indicate closing and opening hours of restaurant

--- bookingsystem/Classes/PortalClasses/restaurant_info_updater.py
+++ bookingsystem/Classes/PortalClasses/restaurant_info_updater.py
@@ -7,8 +7,17 @@ class RestaurantInfoUpdater():
         current_user = request.user.id
         restaurant_id = UserRestaurantLink.objects.filter(user_id=current_user).values_list('restaurant_id',
                                                                                             flat=True).first()
+        day_array = ['open_monday', 'open_tuesday', 'open_wednesday', 'open_thursday', 'open_friday', 'open_saturday',
+                     'open_sunday']
+        database_updater = DatabaseUpdater()
         for response in request.POST:
             if response != 'csrfmiddlewaretoken':
                 column_name = response
                 column_value = request.POST[response]
-                DatabaseUpdater().update_model_instance(Restaurants, restaurant_id, restaurant_id, column_name, column_value)
+                database_updater.update_model_instance(Restaurants, restaurant_id, restaurant_id, column_name,
+                                                       column_value)
+                # If value does not appear in request.POST, the value is not checked -> restaurant closed
+                if column_name in day_array: day_array.remove(column_name)
+
+        for closed_day in day_array:
+            database_updater.update_model_instance(Restaurants, restaurant_id, restaurant_id, closed_day, False)
