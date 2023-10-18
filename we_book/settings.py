8ef02aa49a834ac874b9e741e78a7f59288c1414
milovanpinxteren <milo.van.pinxteren@gmail.com>
2023-10-18 15:25:35 +0200

    test: improved postdeploy

--- we_book/settings.py
+++ we_book/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookingsystem.apps.BookingsystemConfig',
+    'bookingsystem',
     'user_agents',
     'mathfilters',
     'corsheaders'
