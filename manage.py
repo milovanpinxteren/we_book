0f8e05a28710dddccb958b8d0ce338b408e720e1
milovanpinxteren <milo.van.pinxteren@gmail.com>
2023-10-18 16:25:47 +0200

    feat: predeploy

--- manage.py
+++ manage.py
@@ -17,9 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    # django.setup()
-    # from bookingsystem.management.commands.generate_menus import GenerateMenus
-    # GenerateMenus().handle()
 
 
 if __name__ == '__main__':
